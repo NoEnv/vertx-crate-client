2a3cb1bffce163c93ca6f06b6bad1b29f3a4d07e
Lukas Prettenthaler <rdx@wsn.at>
2026-02-26 07:19:44 -0700

    add test container

--- src/test/java/com/noenv/crate/CrateClientTest.java
+++ src/test/java/com/noenv/crate/CrateClientTest.java
@@ -1,7 +1,13 @@
 package com.noenv.crate;
 
 import io.vertx.test.core.VertxTestBase;
+import org.junit.Test;
+import org.testcontainers.cratedb.CrateDBContainer;
 
 public class CrateClientTest extends VertxTestBase {
-
+  @Test
+  public void testConnect() {
+    CrateDBContainer cratedb = new CrateDBContainer("crate:6.2.1");
+    cratedb.start();
+  }
 }
